2a76afc886a6d1babfcc2a957bacde5e552432ea
Charlie Ford <charles.ford1978@gmail.com>
2021-09-30 00:12:35 -0400

    Add project files.

---
+++ Banking-Practice/Account.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Practice
+{
+    class Account
+    {
+        public int Id { get; set; }
+        public decimal Balance { get; set; } = 0;
+        public string Description { get; set; }
+
+        public bool Deposit(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Cannot enter 0 or negative numbers!");
+                return false;
+            }
+            this.Balance = this.Balance + amount;
+            return true;
+        }
+
+        public bool Withdraw(int amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Cannot enter 0 or negative numbers!");
+                return false;
+            }
+            if (amount > this.Balance)
+            {
+                Console.WriteLine("Insufficient funds!");
+                return false;
+            }
+            this.Balance = this.Balance - amount;
+            return true;
+        }
+
+        public bool Transfer(int amount, Account ToAccount)
+        {
+            var success = this.Withdraw(amount);
+            if (success == true)
+            {
+                ToAccount.Deposit(amount);
+                return true;
+            }
+            else return false;
+        }
+        
+        public void Print()
+        {
+            Console.WriteLine($"{Id} | {Balance} | {Description}");
+        }
+    }
+}
---
+++ Banking-Practice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Banking_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var acct1 = new Account();
+            acct1.Id = 1;
+            acct1.Description = "New Account";
+            //acct1.Print();
+            //acct1.Deposit(10000);
+            //acct1.Print();
+            //acct1.Withdraw(2550);
+            //acct1.Print();
+            //acct1.Withdraw(0);
+            //acct1.Deposit(-50000);
+            //acct1.Withdraw(8000);
+            //acct1.Print();
+
+            var acct2 = new Account();
+            acct2.Id = 2;
+            acct2.Description = "Charlie's Account";
+            acct2.Deposit(10000);
+            acct2.Print();
+            acct1.Deposit(10000);
+            acct1.Print();
+            acct1.Transfer(5000, acct2);
+            acct1.Print();
+            acct2.Print();
+        }
+    }
+}
